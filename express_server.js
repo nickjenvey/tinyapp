28785b93d225ebef3d2ca233aea0fe053082df75
Nick <nbjenvey@gmail.com>
2019-06-04 21:23:37 +0000

    Included a route for adding new URLs, added body-parser, added generateRandomString function

--- express_server.js
+++ express_server.js
@@ -4,6 +4,13 @@ var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  return Math.random().toString(36).replace('0.', '').substr(0, 6);
+}
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -30,6 +37,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
   let templateVars = {
@@ -37,4 +48,9 @@ app.get("/urls/:shortURL", (req, res) => {
     longURL: urlDatabase[shortURL]
   };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
