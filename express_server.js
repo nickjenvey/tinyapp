8f10bd3b7d1bda20baf2448167e295fcd2f3d719
Nick <nbjenvey@gmail.com>
2019-06-04 22:02:34 +0000

    Fixed a small error

--- express_server.js
+++ express_server.js
@@ -42,10 +42,9 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let shortURL = req.params.shortURL;
   let templateVars = {
-    shortURL: shortURL,
-    longURL: urlDatabase[shortURL]
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
   };
   res.render("urls_show", templateVars);
 });
