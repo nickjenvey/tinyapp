4a6f9068ced24a8c7a21a7595451843695438fd7
Nick <nbjenvey@gmail.com>
2019-06-05 21:40:22 +0000

    Got cookies working

--- express_server.js
+++ express_server.js
@@ -7,7 +7,10 @@ app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-const generateRandomString = () => Math.random().toString(36).replace('0.', '').substr(0, 6);
+const generateRandomString = () => Math.random().toString(36).replace("0.", "").substr(0, 6);
+
+const cookieParser = require("cookie-parser")
+app.use(cookieParser())
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,22 +25,44 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// Set the cookie
+app.post("/login", (req, res) => {
+  const login = req.body.username;
+  if (login) {
+    res.cookie("username", login);
+  }
+  res.redirect("/urls");
+});
+
+// Delete the cookie
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
+
 // Index Page
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 // Create new URL page
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  }
+  res.render("urls_new", templateVars);
 });
 
 // Access individual URL page
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL]
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
   };
   if (templateVars.longURL) {
     res.render("urls_show", templateVars);
